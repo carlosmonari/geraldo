da561976b3d651077dcc87e7438edfc584788d6b
thiagopena <thiagovilelap@hotmail.com>
2016-11-30 18:22:03 -0200

    deletando exemplos, docs, etc..

--- site/newsite/django_1_0/tests/regressiontests/file_uploads/models.py
+++
@@ -1,9 +0,0 @@
-import tempfile
-import os
-from django.db import models
-
-UPLOAD_ROOT = tempfile.mkdtemp()
-UPLOAD_TO = os.path.join(UPLOAD_ROOT, 'test_upload')
-
-class FileModel(models.Model):
-    testfile = models.FileField(upload_to=UPLOAD_TO)
--- site/newsite/django_1_0/tests/regressiontests/utils/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-from . import views
-
-urlpatterns = patterns('',
-    (r'^xview/$', views.xview),
-)
